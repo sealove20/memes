d4b6087255df6ac441e455078179b14dbea7b13d
Matheus Henrique <matheus.181137@gmail.com>
2021-08-22 00:14:49 -0400

    feat: create state scaffold

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./styles.css";
 
 export const Home = () => {
+  const [homeState, setHomeState] = useState("");
+
+  function toPickle() {
+    setHomeState("Pickles");
+  }
+
+  function toHotDog() {
+    setHomeState("Hot Dog");
+  }
+
   return (
     <div className="layout">
       <div aria-label="card" className="card">
-        <h1>Hello</h1>
+        <div className="card__content">
+          <h1>Hello</h1>
+          <p>{homeState}</p>
+          <button onClick={toPickle}>Pickle</button>
+          <button onClick={toHotDog}>Hot dog</button>
+        </div>
       </div>
     </div>
   );
