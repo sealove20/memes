ea78bc9b87ac041b79e46f67b87a83d2713b4975
Matheus Henrique <matheus.181137@gmail.com>
2021-08-21 23:30:41 -0400

    feat: create a card in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,9 @@ import "./styles.css";
 export const Home = () => {
   return (
     <div className="layout">
-      <h1>Hello</h1>
+      <div aria-label="card" className="card">
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 };
