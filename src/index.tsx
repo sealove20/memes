404d68089ce89c71f7b9297b0bd27337f8afe64c
Matheus Henrique <matheus.181137@gmail.com>
2021-08-24 00:24:25 -0400

    feat: create redux provider container

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import { Provider } from "react-redux";
-import store from "./store/reducer";
+import createSagaMiddleware from "redux-saga";
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./store/reducer";
+import rootSaga from "./store/sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
